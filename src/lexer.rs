c7ebca6386863b0df7e1bffd75dec13b70bbdfb9
RobinCamarasa <robin.camarasa@pm.me>
2024-09-14 21:02:20 +0200

    Start lexer code

---
+++ src/lexer.rs
@@ -0,0 +1,50 @@
+use rstest::rstest;
+use anyhow::{Result,anyhow};
+use logos::Logos;
+
+#[derive(Logos, Debug, PartialEq)]
+enum Token {
+    #[token("'")]
+    StartLineComment,
+
+    #[token("\n", priority = 3)]
+    EOL,
+
+    #[token("[[")]
+    StartBlock,
+
+    #[token("]]")]
+    EndBlock,
+
+    #[regex(r"FILES")]
+    FilesKeyword,
+
+    #[regex(r"=")]
+    Assignment,
+
+    #[regex(r"(\./|/)[a-zA-Z0-9_]+\.[a-z]+")]
+    FileName,
+
+    #[token(";")]
+    Semicolon,
+
+    #[regex(r"[ \t\n\r]+")]
+    Whitespace,
+
+    #[regex(r"[a-zA-Z0-9_]+\.[a-z]+")]
+    Litteral,
+}
+
+#[rstest]
+#[case("' [[ FILES = ./bowser.puml ./wario.puml ./waluigi.puml;]]")]
+fn test_individual_parsing(#[case] case: &str) -> Result<()> {
+    let mut lexer = Token::lexer(case);
+
+    while let Some(token) = lexer.next() {
+        match token {
+            Ok(_) => println!("{:?}: {:?}", token, lexer.slice()),
+            _ => return Err(anyhow!("Could not parse {:?} in {:?}", lexer.slice(), &case)),
+        }
+    }
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod lexer;
+
+fn main() {
+    println!("This project is in construction. Please contact Robin Camarasa <robin.camarasa@pm.me> if you are interested or want to contribute ☺️ ");
+}
